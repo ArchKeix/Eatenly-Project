8434596b9cab14ab5a6c2942a3dc6b41ebd625dc
Keino Aflah <aflah.zahiry@gamil.com>
2025-11-12 00:02:05 +0700

    update sumber data model gemini

---
+++ Backend/db_conf.py
@@ -0,0 +1,45 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
+
+# Url MongoDB dari .env
+uri = os.getenv("mongo_url")
+
+# Connect to MongoDB
+client = MongoClient(uri)
+
+# Database Eatenly_Project
+db = client["Eatenly_Project"]
+
+# Collection Eatenly_Project
+users_collect = db["user"]
+produk_collect = db["produk"]
+kondisi_kesehatan_collect = db["kondisi_kesehatan"]
+riwayat_analisis_collection = db["riwayat_analisis"]
+preferensi_collect = db["preferensi"]
+
+preferensi_pengguna_collect = db["preferensi_pengguna"]
+profile_kesehatan_pengguna_collection = db["profile_kesehatan_pengguna"]
+
+# Test connection
+try:
+    client.admin.command("ping")
+    print("Connected to MongoDB successfully!")
+
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
+
+# Test CRUD
+result = riwayat_analisis_collection.insert_one(
+    {
+        "id_riwayat": 12,
+        "hasil_analisa": "produk yupi ga aman dikonsumsi oleh penderita diabetes",
+        "rekomendasi_produk": "Produk ini tidak aman dikonsumsi oleh penderita diabetes. Sebaiknya pilih produk dengan kandungan gula yang lebih rendah atau tanpa gula tambahan.",
+    }
+)
+
+r = users_collect.find()
+print(list(r))
--- Backend/services_ai.py
+++ Backend/services_ai.py
@@ -1,42 +1,71 @@
-# Langchain 
+# ----------------------------------------------------------------------- #
+#                               LIBRARIES                                 #
+# ----------------------------------------------------------------------- #
+
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.messages import HumanMessage
-from google.genai import types
-import base64
-
-# Load env
+from langchain.messages import SystemMessage, HumanMessage
+from google.ai.generativelanguage_v1beta.types import Tool as GenAITool
 from dotenv import load_dotenv
-import os 
-
-# Cleaning text
+import base64
+import os
 import re
 
-# Load.env
+# ----------------------------------------------------------------------- #
+# Load .env
 load_dotenv()
-
-# Mengambil API key dari .env
 os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
 
-# Inisialisasi model
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+# ----------------------------------------------------------------------- #
 
-with open("produk2.png", "rb") as f:
+# Inisialisasi model
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.6,
+    max_output_tokens=1500,
+)
+
+
+# ----------------------------------------------------------------------- #
+# Encode gambar ke base64
+with open("produk3.png", "rb") as f:
     img_b64 = base64.b64encode(f.read()).decode("utf-8")
 
-# Setup Task 
-message = [
+# ----------------------------------------------------------------------- #
+# Setup Pesan dan Task
+messages = [
+    SystemMessage(
+        content=(
+            """
+            Anda adalah asisten AI ahli dalam menganalisis produk konsumsi kemasan dari gambar.
+            Gunakan framework berikut untuk menjawab:
+            1. Kandungan gizi/komposisi produk
+            2. Kesehatan komposisi produk
+            3. Rekomendasi konsumsi produk
+            4. List referensi sumber jawaban (Sumber: scholar.google.com, sciencedirect.com, nature.com, atau nih.gov) 
+               dengan format nomor urut dan link
+
+            Note:
+            - Jika tidak ada nama merek, buat dugaan berdasarkan visual kemasan.
+            - Hasil teks analisa harus rapi, tanpa simbol aneh seperti *, -, #, [], dll.
+            - Gunakan referensi dari jurnal ilmiah 3 tahun terakhir
+            - Berikan hasil analisa ringkas tapi lengkap, detail, dan valid.
+            """
+        )
+    ),
     HumanMessage(
         content=[
-            {"type": "text", "text": "Jelaskan isi gambar ini beserta komposisi bahannya serta apakah halal."},
-            {"type": "image_url", "image_url": f"data:image/png;base64,{img_b64}"}
+            {"type": "text", "text": "Analisa produk berikut:"},
+            {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{img_b64}"},
+            },
         ]
-    )
+    ),
 ]
 
-# Setup respon
-response = model.invoke(message)
-clean_response = re.sub(r"\*+", "",response.content)  # hapus semua tanda *
-clean_response = clean_response.strip()  # hapus spasi awal/akhir
-
-# Tampilkan respon
-print(clean_response)
+# ----------------------------------------------------------------------- #
+# Jalankan model
+response = model.invoke(messages)
+response = re.sub(r"\*+", "", response.content)
+response = response.strip()
+print(response)
